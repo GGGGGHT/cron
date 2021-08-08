86eed7db47a9f9b57e31817640c0fe6bfd93b139
GitHub Action <action@github.com>
2021-08-08 01:35:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/ggggght/cron/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ggggght/cron/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ggggght.cron.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ggggght.cron.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ggggght/cron/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ggggght/cron/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ggggght.cron.services
 
+import com.github.ggggght.cron.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
